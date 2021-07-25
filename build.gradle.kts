91d44c7b6583603df69b62ea983f9299bef0562e
josephk <87587150+joseph2021k@users.noreply.github.com>
2021-07-25 04:25:40 +0200

    Fix Mockito cannot mock this class errors
    With JDK 11 use mockito-core:2.22.0 (https://stackoverflow.com/a/58488444)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 
     testImplementation (platform("org.junit:junit-bom:5.4.2"))
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
-    testImplementation ("org.mockito:mockito-core:2.7.22")
+    testImplementation ("org.mockito:mockito-core:2.22.0")
 
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine")
 }
